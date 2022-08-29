cce958fc3062982d9b81970daef37a2cc869dad7
Pedro Bigas <91469236+PedroBigas@users.noreply.github.com>
2022-08-29 11:08:42 -0300

    todo-list com modal finalizado

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { BsFillPlusCircleFill } from "react-icons/bs";
 import './Todo.css'
 import List from "./components/List";
 import TodoForm from "./components/TodoForm";
 import Item from './components/Item.js'
+import Modal from "./components/Modal";
+
+const SAVED_ITEMS = 'savedItems'
 
 function Todo() {
+
+    const [showModal, setShowModal] = useState(false)
     const [items, setItems] = useState([]);
+
+    useEffect(()=>{
+
+        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS))
+
+        if(savedItems){
+            setItems(savedItems);
+        }
+    }, [])
+
+    useEffect(()=>{
+        localStorage.setItem(SAVED_ITEMS, JSON.stringify(items))
+    }, [items])
 
 
     function onAddItem(text){
@@ -13,6 +32,7 @@ function Todo() {
         let it = new Item(text);
 
         setItems([...items, it])
+        onHideModal()
     }
 
     function onItemDeleted(item){
@@ -33,14 +53,19 @@ function Todo() {
         setItems(updatedItems)
     }
 
+    function onHideModal(){
+        setShowModal(false)
+    }
 
     return(
 
         <div className="container">    
-            <h1>Todo</h1>
-            <TodoForm onAddItem={onAddItem}></TodoForm>
+            <header className="header"><h1>Todo</h1> <button onClick={()=>{setShowModal(true)}} className="addButton"><BsFillPlusCircleFill></BsFillPlusCircleFill></button></header>    
+            
 
             <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
+
+            <Modal show={showModal} onHideModal={onHideModal}><TodoForm onAddItem={onAddItem}></TodoForm></Modal>
         </div>
     )
 }
